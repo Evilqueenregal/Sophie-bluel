1f731c5c1f2b97512b272c86608e54cdf1b2bbf6
sev <lorquangel@hotmail.fr>
2024-02-12 14:44:25 +0100

    integration modale 2

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,12 +1,17 @@
 let modal = null
+const focusableSelector = 'button, a, input, textarea'
+let focusables = []
+let previouslyFocusedElement = null
 
 const openModal = function (e) {
     e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute('href'))
-    target.style.display = null;
-    target.removeAttribute('aria-hidden')
-    target.setAttribute('aria-modal', 'true')
-    modal = target
+    modal = document.querySelector(e.target.getAttribute('href'))
+    focusables = Array.from(modal.querySelectorAll(focusableSelector))
+    previouslyFocusedElement = document.querySelector(':focus')
+    focusables[0].focus()
+    modal.style.display = null;
+    modal.removeAttribute('aria-hidden')
+    modal.setAttribute('aria-modal', 'true')
     modal.addEventListener('click', closeModal)
     modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
     modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
@@ -14,7 +19,12 @@ const openModal = function (e) {
 
 const closeModal = function (e) {
     if (modal === null) return
+    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus()
     e.preventDefault()
+    window.setTimeout(function() {
+        modal.style.display = "none"
+        modal = null
+    }, 500)
     modal.style.display = "none"
     modal.setAttribute('aria-hidden', 'true')
     modal.removeAttribute('aria-modal')
@@ -28,6 +38,23 @@ const stopPropagation = function (e) {
     e.stopPropagation()
 }
 
+const focusInModal = function (e) {
+    e.preventDefault()
+    let index = focusables.findIndex(f => f === modal.querySelector(':focus'))
+    if (e.shiftKey === true) {
+        index--
+    }else {
+        index++
+    }
+    if (index >= focusables.length) {
+        index = 0
+    }
+    if (index < 0) {
+        index = focusables.length - 1
+    }
+    focusables[index].focus()
+}
+
 document.querySelectorAll('.js-modal').forEach(a => {
     a.addEventListener('click', openModal)
 })
@@ -36,4 +63,6 @@ window.addEventListener('keydown', function (e) {
     if (e.key === "Escape" || e.key === "Esc") {
         closeModal(e)
     }
+    if (e.key === 'Tab' && modal !== null)
+        focusInModal(e)
 })
